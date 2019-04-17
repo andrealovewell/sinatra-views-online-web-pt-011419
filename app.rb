b1075b2d97ed7ef6bbb17e7c34b909a4fcb65571
Andrea Lovewell <andy.lovewell@gmail.com>
2019-04-17 05:28:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+    "<h1> Hello World. This HTML code is inside of a '.erb' file </h1>"
 	end
+
+  get "/index" do
+      erb :index
+    end
+
+    get "/info" do
+     erb :info
+   end
 end
